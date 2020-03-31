798e73663186d724b05b9c58a6790584b09c07d5
Shubhang-dot <55317255+Shubhang-dot@users.noreply.github.com>
2020-03-31 18:31:05 +0530

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,12 @@
+package GuiTutorial;
+
+import javax.swing.*;
+
+public class Frame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JFrame");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+    }
+}
